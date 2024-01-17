4bacaa3fc299243c618936073bd83c218c21facc
Ferdynand Odhiambo <116783408+MeFerdi@users.noreply.github.com>
2024-01-17 14:43:28 +0300

    Update exercise2.py
    fix

--- exercise2.py
+++ exercise2.py
@@ -1,15 +1,24 @@
 import re
 
-def extract_numbers_from_file(filename):
+def find_single_valid_number(line):
+    
+    match = re.match(r"^\s*([-+]?\d*\.\d+|[-+]?\d+)\s*$", line)
+    
+    if match:
+        return float(match.group(1))
+    else:
+        return None
 
+def extract_numbers_from_file(filename):
     numbers = []
 
     with open(filename, 'r') as file:
-
         for line in file:
+        
+            number = find_single_valid_number(line)
+            
+            # Add the valid number to the list
+            if number is not None:
+                numbers.append(number)
 
-            matches = re.findall(r'-?\d+(?:\.\d+)?(?:[eE]-?\d+)?', line)#used regular expression to match valid numbers
-
-            numbers.extend([float(match) for match in numbers])
-
-            return numbers
+    return numbers
