b3cf158bbb5f932ca5085e2d46a5d05b09cd4aa7
Ferdynand Odhiambo <116783408+MeFerdi@users.noreply.github.com>
2024-01-17 14:44:21 +0300

    Update exercise3.py
    fix

--- exercise3.py
+++ exercise3.py
@@ -1,10 +1,23 @@
-def max_distinct_gifts(arr):
-    gift_index = {}
-    start = max_distinct = 0
+def min_tables_to_grab_gifts(gifts):
+    gift_positions = {}
+    max_gift_count = 0
+    start_table = 0
+    end_table = 0
 
-    for end, gift in enumerate(arr):
-        start = max(start, gift_index.get(gift, 0) + 1)
-        gift_index[gift] = end
-        max_distinct = max(max_distinct, end - start + 1)
+    current_start = 0
+    current_gift_count = 0
 
-    return max_distinct
+    for i, gift in enumerate(gifts):
+        if gift in gift_positions and gift_positions[gift] >= current_start:
+            current_start = gift_positions[gift] + 1
+            current_gift_count = i - current_start
+
+        gift_positions[gift] = i
+        current_gift_count += 1
+
+        if current_gift_count > max_gift_count:
+            max_gift_count = current_gift_count
+            start_table = current_start
+            end_table = i
+
+    return start_table, end_table, gifts[start_table:end_table+1]
